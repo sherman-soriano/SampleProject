6fc91539776ef443a820573826d164a563e01b19
sherman-soriano <sherman.soriano@pointwest.com.ph>
2017-11-21 21:16:38 +0800

    new bean

---
+++ Employee.java
@@ -0,0 +1,26 @@
+public class Employee{
+	private int id;
+	private String name;
+	
+	public Employee(){
+		this.id = 1;
+	}
+	
+	public void setId(int id);
+		this.id = id;
+	}
+	
+	public int getId(){
+		return id;
+		
+	}
+	
+	public void setName(String name);
+		this.name = name;
+	}
+	
+	public int getName(){
+		return name;
+		
+	}
+}
